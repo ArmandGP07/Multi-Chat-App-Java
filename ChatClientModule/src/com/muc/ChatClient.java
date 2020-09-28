fd8a77821f586b2df28eec393713b739b56ee047
Armando García Paniagua <armandgp07@gmail.com>
2020-09-27 23:48:34 -0600

    Multi-Chat-App-Java
    Added:
    - User Online/Offline API methods

--- ChatClientModule/src/com/muc/ChatClient.java
+++ ChatClientModule/src/com/muc/ChatClient.java
@@ -1,7 +1,10 @@
 package com.muc;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 // Chat client class for interface with the server //
 public class ChatClient {
@@ -14,16 +17,31 @@ public class ChatClient {
     private OutputStream serverOut;
     private BufferedReader bufferedIn;
 
+    private ArrayList<UserStatusListener> userStatusListeners = new ArrayList<>();
 
-    public ChatClient(String serverName, int serverPort){
+    public ChatClient(String serverName, int serverPort) {
         this.serverName = serverName;
         this.serverPort = serverPort;
     }
+
     public static void main(String[] args) throws IOException {
 
         // Client instance //
         ChatClient client = new ChatClient("localhost", 8818);
-        if(!client.connect()) {
+        client.addUserStatusListener(new UserStatusListener() {
+            @Override
+            public void online(String login) {
+                System.out.println("ONLINE: " + login);
+
+            }
+
+            @Override
+            public void offline(String login) {
+                System.out.println("OFFLINE: " + login);
+
+            }
+        });
+        if (!client.connect()) {
             System.err.println("Connection failed");
         } else {
             System.out.println("Connection successful");
@@ -35,8 +53,11 @@ public class ChatClient {
             } else {
                 System.err.println("Login failed");
             }
+
+            client.logoff();
         }
     }
+
 
     // API login Method //
     private boolean login(String login, String password) throws IOException {
@@ -47,9 +68,68 @@ public class ChatClient {
         System.out.println("Response Line: " + response);
 
         if ("login successful".equalsIgnoreCase(response)) {
+            startMessageReader();
             return true;
         } else {
             return false;
+        }
+    }
+
+    // Logoff method //
+    private void logoff() throws IOException {
+        String cmd = "logoff\n";
+        serverOut.write(cmd.getBytes());
+    }
+
+    // Message Reader method //
+    private void startMessageReader() {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                readMessageLoop();
+            }
+        };
+        t.start();
+    }
+
+    // Message reading loop method //
+    private void readMessageLoop() {
+        try {
+            String line;
+            while ((line = bufferedIn.readLine()) != null) {
+                String[] tokens = StringUtils.split(line);
+                if (tokens != null && tokens.length > 0) {
+                    String cmd = tokens[0];
+                    if ("online".equalsIgnoreCase(cmd)) {
+                        handleOnline(tokens);
+                    } else if ("offline".equalsIgnoreCase(cmd)) {
+                        handleOffline(tokens);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Method for handling offline status //
+    private void handleOffline(String[] tokens) {
+        String login = tokens[1];
+        for (UserStatusListener listener : userStatusListeners) {
+            listener.offline(login);
+        }
+    }
+
+    // Method for handling online status //
+    private void handleOnline(String[] tokens) {
+        String login = tokens[1];
+        for (UserStatusListener listener : userStatusListeners) {
+            listener.online(login);
         }
     }
 
@@ -66,5 +146,12 @@ public class ChatClient {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public void addUserStatusListener(UserStatusListener listener) {
+        userStatusListeners.add(listener);
+    }
+    public void removedUserStatusListener(UserStatusListener listener) {
+        userStatusListeners.remove(listener);
     }
 }
